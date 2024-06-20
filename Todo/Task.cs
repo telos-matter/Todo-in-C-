f4c994057f01fba93af5bc3d11593ea3587700a8
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-06-20 13:35:18 +0200

    Update Task.cs

--- Todo/Task.cs
+++ Todo/Task.cs
@@ -2,56 +2,64 @@ using System.Text;
 
 class Task
 {
-
-    private string prompt;
-    private bool completed;
-    private readonly DateTime creationDT;
+    private readonly int _id;
+    private string _prompt;
+    private bool _completed;
+    private readonly DateTime _creationDT;
     /*
     When should this task be finished,
     or when is it due,
     or when is it happening,
     or something similar.
     */
-    private DateTime? associatedDT; 
+    private DateTime? _associatedDT; 
 
-    public Task (string prompt, DateTime? associatedDT)
+    public Task (int id, string prompt, DateTime? associatedDT)
     {
-        this.prompt = prompt;
-        completed = false;
-        creationDT = DateTime.Now;
-        this.associatedDT = associatedDT;
+        _id = id;
+        _prompt = prompt;
+        _completed = false;
+        _creationDT = DateTime.Now;
+        _associatedDT = associatedDT;
+    }
+
+    public int Id
+    {
+        get => _id;
     }
 
     public string Prompt 
     {
-        get => prompt;
-        set {prompt = value;}
+        get => _prompt;
+        set {_prompt = value;}
     }
 
     public bool Completed
     {
-        get => completed;
-        set {completed = value;}
+        get => _completed;
+        set {_completed = value;}
     }
 
     public DateTime CreationDT
     {
-        get => creationDT;
+        get => _creationDT;
     }
 
     public DateTime? AssociatedTD
     {
-        get => associatedDT;
-        set {associatedDT = value;}
+        get => _associatedDT;
+        set {_associatedDT = value;}
     }
 
+    // TODO more user friendly ToString
     public override string ToString()
     {
         StringBuilder sb = new();
-        sb.Append($"Prompt: `{prompt}`\n");
-        sb.Append($"Completed: {completed}\n");
-        sb.Append($"CreationDT: {creationDT}\n");
-        sb.Append($"AssociatedDT: {associatedDT}");
+        sb.Append($"ID: {_id}\n");
+        sb.Append($"Prompt: `{_prompt}`\n");
+        sb.Append($"Completed: {_completed}\n");
+        sb.Append($"CreationDT: {_creationDT}\n");
+        sb.Append($"AssociatedDT: {_associatedDT}");
         sb.Replace("\n", Environment.NewLine);
         return sb.ToString();
     }
