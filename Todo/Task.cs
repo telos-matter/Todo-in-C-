b7904c4ffe86b108e3c806ef53203d2cf6c4169d
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-06-21 20:12:55 +0200

    Update Task.cs
    We can now serialize

--- Todo/Task.cs
+++ Todo/Task.cs
@@ -1,10 +1,15 @@
 using System.Text;
+using System.Text.Json.Serialization;
 
 class Task
 {
+    [JsonInclude]
     private readonly int _id;
+    [JsonInclude]
     private string _prompt;
+    [JsonInclude]
     private bool _completed;
+    [JsonInclude]
     private readonly DateTime _creationDT;
     /*
     When should this task be finished,
@@ -12,7 +17,17 @@ class Task
     or when is it happening,
     or something similar.
     */
+    [JsonInclude]
     private DateTime? _associatedDT; 
+
+    [JsonConstructor]
+    private Task (int _id, string _prompt, bool _completed, DateTime _creationDT, DateTime? _associatedDT) {
+        this._id = _id;
+        this._prompt = _prompt;
+        this._completed = _completed;
+        this._creationDT = _creationDT;
+        this._associatedDT = _associatedDT;
+    }
 
     public Task (int id, string prompt, DateTime? associatedDT)
     {
@@ -23,28 +38,33 @@ class Task
         _associatedDT = associatedDT;
     }
 
+    [JsonIgnore]
     public int Id
     {
         get => _id;
     }
 
+    [JsonIgnore]
     public string Prompt 
     {
         get => _prompt;
         set {_prompt = value;}
     }
 
+    [JsonIgnore]
     public bool Completed
     {
         get => _completed;
         set {_completed = value;}
     }
 
+    [JsonIgnore]
     public DateTime CreationDT
     {
         get => _creationDT;
     }
 
+    [JsonIgnore]
     public DateTime? AssociatedTD
     {
         get => _associatedDT;
